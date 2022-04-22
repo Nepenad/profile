0c0292e89a79cf3bd363ebbb8c429e6088c5ff9f
Nepenad <77262848+Nepenad@users.noreply.github.com>
2022-04-22 22:42:36 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+            'seafoam': '#243c5a',
+        },
+      },
+    },
+    plugins: [],
+  }
